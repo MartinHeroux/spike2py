34a12144dcb0cde63f2a8abe6ca85d3f623bad8f
Martin Heroux <heroux.martin@gmail.com>
2020-08-07 14:59:22 +1000

    Update trials.py and channels.py to process trial

--- spike2py/trial.py
+++ spike2py/trial.py
@@ -1,24 +1,33 @@
 import os
 
+from spike2py import read, channels
+
+
+channel_generator = {'event': channels.Event,
+                     'keyboard': channels.Keyboard,
+                     'waveform': channels.Waveform,
+                     'wavemark': channels.Wavemark,
+                     }
+
 
 class Trial:
     """Class for experimental trial recorded using Spike2."""
 
-    def __init__(self, file, name=None, subject_id=None, channels=None):
+    def __init__(self, file, channels=None, name=None, subject_id=None):
         """
 
         Parameters
         ----------
         file: str
             Absolute path to data file. Only .mat files are currently supported.
-        name: str (Default: name of file)
-            Descriptive name of trial.
-        subject_id: str
-            Subject's study identifier
         channels: list
             List of channel names, as they appeared in the original .smr file.
             Example: ['biceps', 'triceps', 'torque']
             If not included, all channels will be processed.
+        name: str (Default: name of file)
+            Descriptive name of trial.
+        subject_id: str
+            Subject's study identifier
 
         """
         if name is None:
@@ -26,10 +35,16 @@ class Trial:
         self.name = name
         self.file = file
         self.subject_id = subject_id
-        self.channels = channels
         self._import_trial_data(file, channels)
 
-    def _import_trial_data(self):
-        pass
+    def __repr__(self):
+        return (f'Trial(file={repr(self.file)}, \n\tchannels={self.channels}, '
+                f'\n\tname={self.name}, \n\tsubject_id={self.subject_id}\n\t)')
 
-
+    def _import_trial_data(self, file_name, channel_names):
+        trial_data = read.read(file_name, channel_names)
+        channel_names = list()
+        for key, value in trial_data.items():
+            channel_names.append((key, value['ch_type']))
+            setattr(self, key, channel_generator[value['ch_type']](key, value))
+        self.channels = channel_names
