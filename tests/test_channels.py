842d6380348f390452a7f977c58c22c0e195a027
Martin Heroux <heroux.martin@gmail.com>
2020-08-09 18:33:41 +1000

    Fix tests to be flake8 compliant, to use pathlib and to use new fixture

--- tests/test_channels.py
+++ tests/test_channels.py
@@ -1,36 +1,35 @@
 from spike2py import channels
 
 
-def test_channels_event_init(channels_init):
+def test_channels_event_init(channels_init, channels_mock):
     event = channels.Event(**channels_init['event'])
-    assert event.details.name == channels_init['event']['details'].name
-    assert event.details.trial == channels_init['event']['details'].trial
-    assert list(event.times) == list(channels_init['event']['times'])
+    assert event.details.name == channels_mock['event']['details'].name
+    assert list(event.times) == list(channels_mock['event']['times'])
 
 
-def test_channels_keyboard_init(channels_init):
+def test_channels_keyboard_init(channels_init, channels_mock):
     keyboard = channels.Keyboard(**channels_init['keyboard'])
-    assert keyboard.details.name == channels_init['keyboard']['details'].name
-    assert keyboard.details.trial == channels_init['keyboard']['details'].trial
-    assert list(keyboard.times) == list(channels_init['keyboard']['times'])
-    assert keyboard.codes == channels_init['keyboard']['codes']
+    assert keyboard.details.name == channels_mock['keyboard']['details'].name
+    assert list(keyboard.times) == list(channels_mock['keyboard']['times'])
+    assert keyboard.codes == channels_mock['keyboard']['codes']
 
 
-def test_channels_waveform_init(channels_init):
+def test_channels_waveform_init(channels_init, channels_mock):
     waveform = channels.Waveform(**channels_init['waveform'])
-    assert waveform.details.name == channels_init['waveform']['details'].name
-    assert waveform.details.trial == channels_init['waveform']['details'].trial
-    assert list(waveform.times) == list(channels_init['waveform']['times'])
-    assert waveform.details.units == channels_init['waveform']['details'].units
-    assert list(waveform.values) == list(channels_init['waveform']['values'])
-    assert waveform.details.sampling_frequency == channels_init['waveform']['details'].sampling_frequency
+    assert waveform.details.name == channels_mock['waveform']['details'].name
+    assert list(waveform.times) == list(channels_mock['waveform']['times'])
+    assert waveform.details.units == channels_mock['waveform']['details'].units
+    assert list(waveform.values) == list(channels_mock['waveform']['values'])
+    assert waveform.details.sampling_frequency == \
+           channels_mock['waveform']['details'].sampling_frequency
 
 
-def test_channels_wavemark_init(channels_init):
+def test_channels_wavemark_init(channels_init, channels_mock):
     wavemark = channels.Wavemark(**channels_init['wavemark'])
-    assert wavemark.details.name == channels_init['wavemark']['details'].name
-    assert wavemark.details.trial == channels_init['wavemark']['details'].trial
-    assert list(wavemark.times) == list(channels_init['wavemark']['times'])
-    assert wavemark.details.units == channels_init['wavemark']['details'].units
-    assert wavemark.details.sampling_frequency == channels_init['wavemark']['details'].sampling_frequency
-    assert wavemark.action_potentials == channels_init['wavemark']['action_potentials']
+    assert wavemark.details.name == channels_mock['wavemark']['details'].name
+    assert list(wavemark.times) == list(channels_mock['wavemark']['times'])
+    assert wavemark.details.units == channels_mock['wavemark']['details'].units
+    assert wavemark.details.sampling_frequency == \
+           channels_mock['wavemark']['details'].sampling_frequency
+    assert wavemark.action_potentials == \
+           channels_mock['wavemark']['action_potentials']
