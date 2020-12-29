f1c83bf5bd4f4da205cee1b4d2493245f0c0249a
Martin Heroux <heroux.martin@gmail.com>
2020-12-29 14:21:55 +1100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../"))
+sys.path.insert(0, os.path.abspath("../../spike2py"))
 
 # -- Project information -----------------------------------------------------
 
